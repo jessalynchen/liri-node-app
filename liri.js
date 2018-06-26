2cc27749b998247fca14f131bd127505cb354193
jessalyn <35002728+jessalynchen@users.noreply.github.com>
2018-06-26 19:25:33 -0400

    Add files via upload
    update liri js

--- liri.js
+++ liri.js
@@ -1,5 +1,20 @@
 require("dotenv").config();
 var importKeys = require("keys.js").config();
 
+//access keys information
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+
+//Show the last 20 tweets
+`my-tweets`
+
+//Show the following song in terminal - artist, song name, preview link , albumn of song
+//use the node spotify api
+
+//use the node-spotify-api package
+
+`spotify-this-song`
+
+`movie-this`
+
+`do-what-it-says`
